38eaed3d9276a57bbd59663796c4d0330415afd1
Max Shen <m4xshen@gmail.com>
2023-09-16 20:06:07 +0800

    style: add stylua

--- lua/example/init.lua
+++ lua/example/init.lua
@@ -3,13 +3,13 @@ local M = {}
 function M.setup(opts)
    opts = opts or {}
 
-	vim.keymap.set("n", "<Leader>h", function()
-		if opts.name then
-			print("hello, " .. opts.name)
-		else
-			print("hello")
-		end
-	end)
+   vim.keymap.set("n", "<Leader>h", function()
+      if opts.name then
+         print("hello, " .. opts.name)
+      else
+         print("hello")
+      end
+   end)
 end
 
 return M
